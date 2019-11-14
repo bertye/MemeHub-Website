42375d4f7bd3f06adc00e7b60ceff6fac09fa199
LeifB <leif.bernsdorf@gmail.com>
2019-11-15 00:01:57 +0100

    Using webserver and not dummy data

--- src/votes.js
+++ src/votes.js
@@ -1,11 +1,18 @@
 import { writable } from 'svelte/store';
 import { loading } from './loading.js';
+import { token } from './token.js';
 
 let votes_value = {};
 export const votes = writable(votes_value);
 
-fetch("votes.json")
-    .then(response => response.json())
+fetch('http://localhost:3040/votes?token=' + token)
+    .then(
+        response => response.json(),
+        err => {
+            console.log("ERROR couod not get votes.");
+            console.log(err);
+            return {};
+        })
     .then(json => {
         votes.set(json);
         loading.update(n => n - 1);
@@ -15,15 +22,21 @@ fetch("votes.json")
     });
 
 function sendVotes(votes) {
-    // TODO implement
     loading.update(n => n + 1);
-    console.log("sending votes:");
-    console.log(votes);
-    const req = new Promise(resolve => setTimeout(resolve, 500));
-    req.then(() => {
-        loading.update(n => n - 1);
-    });
-    return req;
+
+    fetch('http://localhost:3040/votes?token=' + token,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(votes)
+        })
+        .then(
+            () => {  },
+            err => {
+                console.log("ERROR could not push votes.");
+                console.log(err);
+            })
+        .then(() => { loading.update(n => n - 1); });
 }
 
 loading.update(n => n + 1);
